ab2e3f1c75d22c21bf728d576674fa8703a83cee
ptitty12 <137674653+ptitty12@users.noreply.github.com>
2024-10-13 16:31:13 -0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -10,7 +10,7 @@ from itertools import product
 getcontext().prec = 50
 
 
-def generate_election_scenarios(df, certainty_threshold=Decimal('0.90')):
+def generate_election_scenarios(df, certainty_threshold=Decimal('0.85')):
     start_time = time.time()
 
     # Convert Odds to Decimal for precision
